6e2093641a42b4772b8687535004769279524bf3
YuvrajArora06 <arorayuvraj14@gmail.com>
2025-11-26 00:34:40 +0530

    DAY -85

---
+++ Q135.c
@@ -0,0 +1,20 @@
+//Assign explicit values starting from 10 and print them.
+#include <stdio.h>
+enum Numbers {
+    TEN = 10,
+    ELEVEN,
+    TWELVE,
+    THIRTEEN,
+    FOURTEEN,
+    FIFTEEN
+};
+int main() {
+    enum Numbers num;
+
+    // Print the values of the enum members
+    for (num = TEN; num <= FIFTEEN; num++) {
+        printf("%d\n", num);
+    }
+
+    return 0;
+}
